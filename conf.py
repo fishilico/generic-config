e9a22f22332fc1ad5a0b9e159fec24d35a1567c2
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2016-08-28 15:14:30 +0200

    Update copyright years

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Generic Config'
-copyright = '2013, Nicolas Iooss'
+copyright = '2013-2016, Nicolas Iooss'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
